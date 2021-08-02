4816ae0be32ae6644b1fd512668e8dc4e16ed370
alejandrodemiquel <alejandrodemiquel@gmail.com>
2021-08-02 13:19:00 +1200

    Many changes
    - Modify complete layout
    - Add comparison between different regional levels

--- app/Election.py
+++ app/Election.py
@@ -1,5 +1,5 @@
 from abc import ABC, abstractmethod
-from collections import Counter
+from collections import Counter, defaultdict
 from datetime import datetime
 import pandas as pd
 import pickle
@@ -46,23 +46,97 @@ class Election(ABC):
         seats_won = Counter()
 
         regions = self.regions()
-        # TODO Regions are different if levels are different
-        for region in regions[system_1['level']]:
-            seat_diff[region.name] = 0
-            system_1_votes = region.compute_election_result(system_1)
-            system_2_votes = region.compute_election_result(system_2)
+
+        if system_1['level']==system_2['level']:
+            for region in regions[system_1['level']]:
+                seat_diff[region.name] = 0
+                system_1_votes = region.compute_election_result(system_1)
+                system_2_votes = region.compute_election_result(system_2)
+                parties = set(system_1_votes.keys()).union(system_2_votes.keys())
+                for p in parties:
+                    if p in system_1_votes and p not in system_2_votes:
+                        seat_diff[region.name] += system_1_votes[p]
+                        seats_won[p] += system_1_votes[p]
+                    elif p in system_2_votes and p not in system_1_votes:
+                        seats_won[p] -= system_2_votes[p]
+                    elif system_1_votes[p]>system_2_votes[p]:
+                        seat_diff[region.name] += system_1_votes[p] - system_2_votes[p]
+                        seats_won[p] += system_1_votes[p] - system_2_votes[p]
+                    else:
+                        seats_won[p] -= system_2_votes[p] - system_1_votes[p]
+
+        elif system_1['level']==0 or system_2['level']==0:
+            seat_diff[self.country.name] = 0
+            if system_1['level']==0:
+                system_1_votes = regions[0][0].compute_election_result(system_1)
+                system_2_votes = Counter()
+                for region in regions[system_2['level']]:
+                    system_2_votes += region.compute_election_result(system_2)
+            else:
+                system_2_votes = regions[0][0].compute_election_result(system_2)
+                system_1_votes = Counter()
+                for region in regions[system_1['level']]:
+                    system_1_votes += region.compute_election_result(system_1)
+
             parties = set(system_1_votes.keys()).union(system_2_votes.keys())
             for p in parties:
                 if p in system_1_votes and p not in system_2_votes:
-                    seat_diff[region.name] += system_1_votes[p]
+                    seat_diff[self.country.name] += system_1_votes[p]
                     seats_won[p] += system_1_votes[p]
                 elif p in system_2_votes and p not in system_1_votes:
                     seats_won[p] -= system_2_votes[p]
                 elif system_1_votes[p]>system_2_votes[p]:
-                    seat_diff[region.name] += system_1_votes[p] - system_2_votes[p]
+                    seat_diff[self.country.name] += system_1_votes[p] - system_2_votes[p]
                     seats_won[p] += system_1_votes[p] - system_2_votes[p]
                 else:
                     seats_won[p] -= system_2_votes[p] - system_1_votes[p]
+            print(seat_diff, seats_won)
+
+        elif system_1['level']==1:
+            system_2_region_votes = defaultdict(Counter)
+            for region in regions[system_2['level']]:
+                votes = region.compute_election_result(system_2)
+                system_2_region_votes[self.superregion[region.name]] += votes
+
+            for region in regions[system_1['level']]:
+                seat_diff[region.name] = 0
+                system_1_votes = region.compute_election_result(system_1)
+                system_2_votes = system_2_region_votes[region.name]
+                parties = set(system_1_votes.keys()).union(system_2_votes.keys())
+                for p in parties:
+                    if p in system_1_votes and p not in system_2_votes:
+                        seat_diff[region.name] += system_1_votes[p]
+                        seats_won[p] += system_1_votes[p]
+                    elif p in system_2_votes and p not in system_1_votes:
+                        seats_won[p] -= system_2_votes[p]
+                    elif system_1_votes[p]>system_2_votes[p]:
+                        seat_diff[region.name] += system_1_votes[p] - system_2_votes[p]
+                        seats_won[p] += system_1_votes[p] - system_2_votes[p]
+                    else:
+                        seats_won[p] -= system_2_votes[p] - system_1_votes[p]
+
+        elif system_2['level']==1:
+            system_1_region_votes = defaultdict(Counter)
+            for region in regions[system_1['level']]:
+                votes = region.compute_election_result(system_1)
+                system_1_region_votes[self.superregion[region.name]] += votes
+
+            for region in regions[system_2['level']]:
+                seat_diff[region.name] = 0
+                system_2_votes = region.compute_election_result(system_2)
+                system_1_votes = system_1_region_votes[region.name]
+                parties = set(system_1_votes.keys()).union(system_2_votes.keys())
+                for p in parties:
+                    if p in system_1_votes and p not in system_2_votes:
+                        seat_diff[region.name] += system_1_votes[p]
+                        seats_won[p] += system_1_votes[p]
+                    elif p in system_2_votes and p not in system_1_votes:
+                        seats_won[p] -= system_2_votes[p]
+                    elif system_1_votes[p]>system_2_votes[p]:
+                        seat_diff[region.name] += system_1_votes[p] - system_2_votes[p]
+                        seats_won[p] += system_1_votes[p] - system_2_votes[p]
+                    else:
+                        seats_won[p] -= system_2_votes[p] - system_1_votes[p]
 
         return {'seat_diff': seat_diff, 'seats_won': seats_won}
 
@@ -95,26 +169,36 @@ class Spain_Election(Election):
         self._regions = parsed_data['regions']
         self._parties = parsed_data['parties']
 
-        self._region_hierarchy = {
-            'Andalucía': ['Almería', 'Cádiz', 'Córdoba', 'Granada', 'Huelva', 'Jaén', 'Málaga', 'Sevilla'],
-            'Aragón': ['Huesca', 'Teruel', 'Zaragoza'],
-            'Cantabria': ['Cantabria'],
-            'Castilla y León': ['Ávila', 'Burgos', 'León', 'Palencia', 'Salamanca', 'Segovia', 'Soria', 'Valladolid', 'Zamora'],
-            'Castilla-La Mancha': ['Albacete', 'Ciudad Real', 'Cuenca', 'Guadalajara', 'Toledo'],
-            'Cataluña': ['Barcelona', 'Girona', 'Lleida', 'Tarragona'],
-            'Ceuta y Melilla': ['Ceuta', 'Melilla'],
-            'Comunidad de Madrid': ['Madrid'],
-            'Comunidad Foral de Navarra': ['Navarra'],
-            'Comunidad Valenciana': ['Alacant', 'Castelló', 'València'],
-            'Extremadura': ['Badajoz', 'Cáceres'],
-            'Galicia': ['A Coruña', 'Lugo', 'Ourense', 'Pontevedra'],
-            'Islas Baleares': ['Illes Balears'],
-            'Islas Canarias': ['Las Palmas', 'Santa Cruz de Tenerife'],
-            'La Rioja': ['La Rioja'],
-            'País Vasco': ['Araba', 'Bizkaia', 'Gipuzcoa'],
-            'Principado de Asturias': ['Asturias'],
-            'Región de Murcia': ['Murcia']
-        }
+        self.superregion = {}
+        for x in ['Almería', 'Cádiz', 'Córdoba', 'Granada', 'Huelva', 'Jaén', 'Málaga', 'Sevilla']:
+            self.superregion[x] = 'Andalucía'
+        for x in ['Huesca', 'Teruel', 'Zaragoza']:
+            self.superregion[x] = 'Aragón'
+        self.superregion['Cantabria'] = 'Cantabria'
+        for x in ['Ávila', 'Burgos', 'León', 'Palencia', 'Salamanca', 'Segovia', 'Soria', 'Valladolid', 'Zamora']:
+            self.superregion[x] = 'Castilla y León'
+        for x in ['Albacete', 'Ciudad Real', 'Cuenca', 'Guadalajara', 'Toledo']:
+            self.superregion[x] = 'Castilla-La Mancha'
+        for x in ['Barcelona', 'Girona', 'Lleida', 'Tarragona']:
+            self.superregion[x] = 'Cataluña'
+        self.superregion['Ceuta'] = 'Ceuta y Melilla'
+        self.superregion['Melilla'] = 'Ceuta y Melilla'
+        self.superregion['Madrid'] = 'Comunidad de Madrid'
+        self.superregion['Navarra'] = 'Comunidad Foral de Navarra'
+        for x in ['Alacant', 'Castelló', 'València']:
+            self.superregion[x] = 'Comunidad Valenciana'
+        self.superregion['Badajoz'] = 'Extremadura'
+        self.superregion['Cáceres'] = 'Extremadura'
+        for x in ['A Coruña', 'Lugo', 'Ourense', 'Pontevedra']:
+            self.superregion[x] = 'Galicia'
+        self.superregion['Illes Balears'] = 'Islas Baleares'
+        self.superregion['Las Palmas'] = 'Islas Canarias'
+        self.superregion['Santa Cruz de Tenerife'] = 'Islas Canarias'
+        self.superregion['La Rioja'] = 'La Rioja'
+        for x in ['Araba', 'Bizkaia', 'Gipuzcoa']:
+            self.superregion[x] = 'País Vasco'
+        self.superregion['Asturias'] = 'Principado de Asturias'
+        self.superregion['Murcia'] = 'Región de Murcia'
 
     def parse_data(self, filename):
         with open(filename, 'rb') as f:
--- app/main.py
+++ app/main.py
@@ -43,7 +43,7 @@ MAPBOX_STYLE = "mapbox://styles/plotlymapbox/cjvprkf3t1kns1cqjxuxmwixz"
 
 CONTENT_STYLE = {
     "position": "relative",
-    "top": 60,
+    "top": 40,
     "bottom": 0,
     "left": 0,
     "right": 0,
@@ -55,6 +55,7 @@ CONTENT_STYLE = {
     "zIndex": 80
 }
 
+"""
 dropdown_countries = dcc.Dropdown(
     id="dropdown-countries",
     options=[
@@ -62,7 +63,37 @@ dropdown_countries = dcc.Dropdown(
     ],
     value='Spain'
 )
+"""
 
+"""
+dropdown_countries = html.Div([
+    html.H6('Country',style={'display':'inline-block'}),
+    dcc.Dropdown(
+        id="dropdown-countries",
+        options=[
+            {'label': 'Spain', 'value': 'Spain'}
+        ],
+        value='Spain'
+    ),
+])
+"""
+
+dropdown_countries = html.Div([
+    dbc.InputGroup([
+        dbc.InputGroupAddon("Country"),
+        dcc.Dropdown(
+            id="dropdown-countries",
+            options=[
+                {'label': 'Spain', 'value': 'Spain'}
+            ],
+            value='Spain',
+            style={"width": "70%"},
+        ),
+    ],
+    )
+])
+
+"""
 dropdown_elections = dcc.Dropdown(
     id="dropdown-elections",
     options=[
@@ -70,7 +101,71 @@ dropdown_elections = dcc.Dropdown(
     ],
     value='2019-11-10'
 )
+"""
 
+
+dropdown_elections = html.Div([
+    dbc.InputGroup([
+        dbc.InputGroupAddon("Election"),
+        dcc.Dropdown(
+            id="dropdown-elections",
+            options=[
+                {'label': '2019-11-10', 'value': '2019-11-10'}
+            ],
+            value='2019-11-10',
+            style={"width": "70%"},
+        ),
+    ],
+    )
+])
+
+"""
+dropdown_elections = html.Div([
+    html.H6('Elections',style={'display':'inline-block'}),
+    dcc.Dropdown(
+        id="dropdown-elections",
+        options=[
+            {'label': '2019-11-10', 'value': '2019-11-10'}
+        ],
+        value='2019-11-10'
+    ),
+])
+"""
+
+"""
+dropdown_metrics = html.Div([
+    html.H6('Metrics',style={'display':'inline-block'}),
+    dcc.Dropdown(
+        id="dropdown-metrics",
+        options=[
+            {'label': 'Seat Difference Percentage', 'value': 'Seat Difference Percentage'},
+            {'label': 'Seat Difference', 'value': 'Seat Difference'},
+            {'label': 'Lost Votes Percentage', 'value': 'Lost Votes Percentage'},
+        ],
+        value='Seat Difference'
+    ),
+])
+"""
+
+dropdown_metrics = html.Div([
+    dbc.InputGroup([
+        dbc.InputGroupAddon("Metrics"),
+        dcc.Dropdown(
+            id="dropdown-metrics",
+            options=[
+                {'label': 'Seat Difference Percentage', 'value': 'Seat Difference Percentage'},
+                {'label': 'Seat Difference', 'value': 'Seat Difference'},
+                {'label': 'Lost Votes Percentage', 'value': 'Lost Votes Percentage'},
+            ],
+            value='Seat Difference',
+            style={"width": "70%"},
+        ),
+    ],
+    )
+])
+
+
+"""
 compare_switch = dbc.Checklist(
     options=[{"label": "Compare", "value": 1}],
     value=[1],
@@ -95,7 +190,9 @@ dropdown_system_2 = dcc.Dropdown(
     ],
     value='SL'
 )
+"""
 
+"""
 dropdown_region_level_1 = dcc.Dropdown(
     id="dropdown-region-level-1",
     options=[
@@ -115,7 +212,9 @@ dropdown_region_level_2 = dcc.Dropdown(
     ],
     value='2'
 )
+"""
 
+"""
 threshold_1 = dcc.Input(
     id="threshold-1",
     type="number",
@@ -129,6 +228,127 @@ threshold_2 = dcc.Input(
     min=0, max=15, step=1,
     value=3
 )
+"""
+
+system_1_card = dbc.Card(
+    [
+        dbc.CardHeader("System 1"),
+        dbc.CardBody([
+            dcc.Dropdown(
+                id="dropdown-system-1",
+                options=[
+                    {'label': "Custom", 'value': 'Custom'},
+                    {'label': "Spain", 'value': 'Spain'},
+                ],
+                value='Custom'
+            ),
+            html.Br(),
+            dbc.Row([
+                dbc.Col(html.P('Name'), width=4),
+                dbc.Col(html.P(), width=2),
+                dbc.Col(html.P('Level'), width=2),
+                dbc.Col(html.P(), width=2),
+                dbc.Col(html.P('Threshold (%)'), width=2),
+            ]),
+            dbc.Row([
+                dbc.Col(
+                    dcc.Dropdown(
+                        id="dropdown-system-name-1",
+                        options=[
+                            {'label': "d'Hondt", 'value': 'dHondt'},
+                            {'label': "Sainte-Laguë", 'value': 'SL'},
+                        ],
+                        value='dHondt'
+                    ),
+                    width=4
+                ),
+                dbc.Col(html.P(), width=2),
+                dbc.Col(
+                    dcc.Dropdown(
+                        id="dropdown-region-level-1",
+                        options=[
+                            {'label': '0', 'value': '0'},
+                            {'label': '1', 'value': '1'},
+                            {'label': '2', 'value': '2'},
+                        ],
+                        value='2'
+                    ),
+                    width=2
+                ),
+                dbc.Col(html.P(), width=2),
+                dbc.Col(
+                    dcc.Input(
+                        id="threshold-1",
+                        type="number",
+                        min=0, max=15, step=1,
+                        value=3
+                    ),
+                    width=2
+                ),
+            ])
+        ]),
+    ],
+)
+
+system_2_card = dbc.Card(
+    [
+        dbc.CardHeader("System 2"),
+        dbc.CardBody([
+            dcc.Dropdown(
+                id="dropdown-system-2",
+                options=[
+                    {'label': "Custom", 'value': 'Custom'},
+                    {'label': "Spain", 'value': 'Spain'},
+                ],
+                value='Custom'
+            ),
+            html.Br(),
+            dbc.Row([
+                dbc.Col(html.P('Name'), width=4),
+                dbc.Col(html.P(), width=2),
+                dbc.Col(html.P('Level'), width=2),
+                dbc.Col(html.P(), width=2),
+                dbc.Col(html.P('Threshold (%)'), width=2),
+            ]),
+            dbc.Row([
+                dbc.Col(
+                    dcc.Dropdown(
+                        id="dropdown-system-name-2",
+                        options=[
+                            {'label': "d'Hondt", 'value': 'dHondt'},
+                            {'label': "Sainte-Laguë", 'value': 'SL'},
+                        ],
+                        value='dHondt'
+                    ),
+                    width=4
+                ),
+                dbc.Col(html.P(), width=2),
+                dbc.Col(
+                    dcc.Dropdown(
+                        id="dropdown-region-level-2",
+                        options=[
+                            {'label': '0', 'value': '0'},
+                            {'label': '1', 'value': '1'},
+                            {'label': '2', 'value': '2'},
+                        ],
+                        value='2'
+                    ),
+                    width=2
+                ),
+                dbc.Col(html.P(), width=2),
+                dbc.Col(
+                    dcc.Input(
+                        id="threshold-2",
+                        type="number",
+                        min=0, max=15, step=1,
+                        value=3
+                    ),
+                    width=2
+                ),
+            ])
+        ]),
+    ],
+)
 
 info_box_1 = dbc.Card(
     [
@@ -140,6 +360,8 @@ info_box_1 = dbc.Card(
         ),
     ],
     style={"width": "18rem"},
+    color='primary',
+    outline=True,
 )
 
 info_box_2 = dbc.Card(
@@ -152,8 +374,41 @@ info_box_2 = dbc.Card(
         ),
     ],
     style={"width": "18rem"},
+    color='primary',
+    outline=True,
 )
 
+content2 = html.Div([
+    dbc.Row([
+        dbc.Col(dropdown_countries, width=3),
+        dbc.Col(html.P(), width=1),
+        dbc.Col(dropdown_elections, width=3),
+        dbc.Col(html.P(), width=1),
+        dbc.Col(dropdown_metrics, width=3),
+    ]),
+    html.Br(),
+    dbc.Row([
+        dbc.Col(system_1_card, width=6),
+        dbc.Col(system_2_card, width=6),
+    ]),
+    html.Br(),
+    dbc.Row([
+        dbc.Col(html.P(), width=1),
+        dbc.Col(info_box_1, width=4),
+        dbc.Col(html.P(), width=2),
+        dbc.Col(info_box_2, width=4),
+        dbc.Col(html.P(), width=1),
+    ]),
+    html.Br(),
+    dbc.Row([
+        dbc.Col(dcc.Graph(id='map'), width=7),
+        dbc.Col(dcc.Graph(id='chart'), width=5)
+    ]),
+    ],
+    style=CONTENT_STYLE
+)
+
+"""
 content = html.Div([
         dbc.Row([
             dbc.Col(dropdown_countries, width=2),
@@ -191,10 +446,11 @@ content = html.Div([
     ],
     style=CONTENT_STYLE
 )
+"""
 
 
 app.layout = html.Div([
-    content
+    content2
 ])
 
 print("Everything is loaded!")
@@ -238,6 +494,7 @@ def set_default_election_values(elections, country):
         Output("map", "figure"),
         Output("chart", "figure"),
         Output('dropdown-system-2', 'disabled'),
+        Output('dropdown-system-name-2', 'disabled'),
         Output('dropdown-region-level-2', 'disabled'),
         Output('threshold-2', 'disabled'),
         Output('card-1-header', 'children'),
@@ -246,11 +503,11 @@ def set_default_election_values(elections, country):
         Output('card-2-title', 'children'),
     ],
     [
-        Input("switch-compare", "value"),
-        Input('dropdown-system-1', 'value'),
+        Input("dropdown-metrics", "value"),
+        Input('dropdown-system-name-1', 'value'),
         Input('dropdown-region-level-1', 'value'),
         Input('threshold-1', 'value'),
-        Input('dropdown-system-2', 'value'),
+        Input('dropdown-system-name-2', 'value'),
         Input('dropdown-region-level-2', 'value'),
         Input('threshold-2', 'value'),
     ],
@@ -258,23 +515,26 @@ def set_default_election_values(elections, country):
         State('dropdown-countries', 'value'),
         State('dropdown-elections', 'value'),
     ])
-def update_maps(compare, system_1, level_1, threshold_1, system_2, level_2, threshold_2, country, elections):
-    level_1, level_2 = int(level_1), int(level_2)
+def update_maps(metric, system_1, level_1, threshold_1, system_2, level_2, threshold_2, country, elections):
 
     election = ELECTIONS[country][elections]
 
     regions = election.regions()
-    locations = [region.name for region in regions[level_1]]
 
-    if compare:
+    if metric.startswith('Seat Difference'):
         disable = False
+
+        level_1, level_2 = int(level_1), int(level_2)
+        level = min(level_1, level_2)
+        locations = [region.name for region in regions[level]]
 
         s1 = {'name': system_1, 'threshold': threshold_1, 'level': level_1}
         s2 = {'name': system_2, 'threshold': threshold_2, 'level': level_2}
         metrics = election.get_compare_metrics(s1, s2)
 
+        #print(metrics['seat_diff'])
         seat_diff = list(metrics['seat_diff'].values())
-        map_fig = go.Figure(go.Choroplethmapbox(geojson=election.country.regions()[level_1]['geojson'],
+        map_fig = go.Figure(go.Choroplethmapbox(geojson=election.country.regions()[level]['geojson'],
                                                 locations=locations,
                                                 z=seat_diff,
                                                 colorscale="Reds", # Options: Greys,YlGnBu,Greens,YlOrR d,Bluered,RdBu,Reds,Blues,Picnic,Rainbow,Portland,Jet,H ot,Blackbody,Earth,Electric,Viridis,Cividis.
@@ -293,11 +553,14 @@ def update_maps(compare, system_1, level_1, threshold_1, system_2, level_2, thre
     else:
         disable = True
 
-        s1 = {'name': system_1, 'threshold': threshold_1, 'level': level_1}
+        level = int(level_1)
+        locations = [region.name for region in regions[level]]
+
+        s1 = {'name': system_1, 'threshold': threshold_1, 'level': level}
         metrics = election.get_single_metrics(s1)
 
         lost_votes_percentage = list(metrics['lost_votes_percentage'].values())
-        map_fig = go.Figure(go.Choroplethmapbox(geojson=election.country.regions()[level_1]['geojson'],
+        map_fig = go.Figure(go.Choroplethmapbox(geojson=election.country.regions()[level]['geojson'],
                                                 locations=locations,
                                                 z=lost_votes_percentage,
                                                 colorscale="Reds",
@@ -324,7 +587,7 @@ def update_maps(compare, system_1, level_1, threshold_1, system_2, level_2, thre
                           mapbox_center = election.country.center(),
                           margin={"r":0,"t":0,"l":0,"b":0})
 
-    return map_fig, bar_fig, disable, disable, disable, card_1_header, card_1_title, card_2_header, card_2_title
+    return map_fig, bar_fig, disable, disable, disable, disable, card_1_header, card_1_title, card_2_header, card_2_title
 
 
 if __name__ == '__main__':
